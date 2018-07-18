7c398b2a6f9f8b5f11c7b3dca24b7287930a236a
Michael-IRL <iszekeell@gmail.com>
2018-07-18 21:55:06 +0100

    Add a class to demo hibernate save

---
+++ hibernate-tutorial/src/main/java/ie/bitterCoffee/hibernate/demo/CreateStudentDemo.java
@@ -0,0 +1,47 @@
+package ie.bitterCoffee.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import ie.bitterCoffee.hibernate.demo.entity.Student;
+
+public class CreateStudentDemo
+{
+
+	public static void main(String[] args)
+	{
+		//Create session factory
+		SessionFactory factory = new Configuration()
+									.configure("hibernate.cfg.xml")
+									.addAnnotatedClass(Student.class)
+									.buildSessionFactory();
+		
+		//Create session
+		Session session = factory.getCurrentSession();
+		
+		try
+		{
+			//create a student object
+			System.out.println("Creating a new student object ...");
+			Student tempStudent = new Student("John","Doe","johndoe@example.ie");
+			
+			//start a transaction
+			session.beginTransaction();
+			
+			//save java object
+			System.out.println("Saving the student ...");
+			session.save(tempStudent);			
+			
+			//commit transaction
+			session.getTransaction().commit();
+			System.out.println("Done!");
+			
+		}
+		finally
+		{
+			factory.close();
+		}		
+	}
+
+}
