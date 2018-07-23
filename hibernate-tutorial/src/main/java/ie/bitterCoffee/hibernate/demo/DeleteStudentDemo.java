bfe62d9cd029c0c8a199d8246d4e3c7c3a1a4f5c
Michael-IRL <iszekeell@gmail.com>
2018-07-23 20:34:45 +0100

    add Delete Student Demo

---
+++ hibernate-tutorial/src/main/java/ie/bitterCoffee/hibernate/demo/DeleteStudentDemo.java
@@ -0,0 +1,51 @@
+package ie.bitterCoffee.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import ie.bitterCoffee.hibernate.demo.entity.Student;
+
+public class DeleteStudentDemo
+{
+
+	public static void main(String[] args)
+	{
+		//Create session factory
+		SessionFactory factory = new Configuration()
+									.configure("hibernate.cfg.xml")
+									.addAnnotatedClass(Student.class)
+									.buildSessionFactory();
+		
+		//Create session
+		Session session = factory.getCurrentSession();
+		
+		try
+		{
+			int studentId = 1;
+			
+			//start a transaction
+			session.beginTransaction();
+			
+			//Retrieve student based on the id: primary key 
+			System.out.println("\nGetting student with id: "+studentId);
+			
+			Student myStudent = session.get(Student.class, studentId);
+			
+			//delete the student
+			System.out.println("Delete the student: "+myStudent);
+			session.delete(myStudent);
+						
+			//commit transaction
+			session.getTransaction().commit();			
+						
+			System.out.println("Done!\n");
+			
+		}
+		finally
+		{
+			factory.close();
+		}		
+	}
+
+}
